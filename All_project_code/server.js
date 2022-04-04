4368fbb7c7c954fb06f4fa342674699045ebbee5
tyra1903 <tyra1903@colorado.edu>
2022-04-04 13:51:29 -0600

    Added npm modules, updated docker-compose(current issue with keeping web server up)

---
+++ All_project_code/server.js
@@ -0,0 +1,52 @@
+/***********************
+  Load Components!
+
+  Express      - A Node.js Framework
+  Body-Parser  - A tool to help use parse the data in a post request
+***********************/
+
+var express = require('express'); //Ensure our express framework has been added
+var app = express();
+var bodyParser = require('body-parser'); //Ensure our body-parser tool has been added
+app.use(bodyParser.json());              // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+//Create Database Connection
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "adl",
+    password: "qwer1234" 
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Database connected.");
+});
+
+
+/**********************
+  Database Connection information
+  host: This defines the ip address of the server hosting our database.
+		We'll be using `db` as this is the name of the postgres container in our
+		docker-compose.yml file. Docker will translate this into the actual ip of the
+		container for us (i.e. can't be access via the Internet).
+  port: This defines what port we can expect to communicate to our database.  We'll use 5432 to talk with PostgreSQL
+  database: This is the name of our specific database.  From our previous lab,
+		we created the football_db database, which holds our football data tables
+  user: This should be left as postgres, the default user account created when PostgreSQL was installed
+  password: This the password for accessing the database. We set this in the
+		docker-compose.yml for now, usually that'd be in a seperate file so you're not pushing your credentials to GitHub :).
+**********************/
+
+
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/'));//This line is necessary for us to use relative paths and access our resources directory
+
+app.listen(3000);
+console.log('3000 is the magic port');
+
+
