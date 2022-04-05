4cb671c39f31f8d2586f810e75d8efcc2c8da55c
tyra1903 <tyra1903@colorado.edu>
2022-04-05 13:57:32 -0600

    Changed node module mysql to mysql2. Web image now running

--- All_project_code/server.js
+++ All_project_code/server.js
@@ -12,34 +12,19 @@ app.use(bodyParser.json());              // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 //Create Database Connection
-var mysql = require('mysql');
+var mysql = require('mysql2');
 
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "adl",
-    password: "qwer1234" 
+    host: "db",
+    user: "root",
+    password: "1234qwer",
+    port: "3306"
 });
 
 con.connect(function(err) {
     if (err) throw err;
     console.log("Database connected.");
 });
-
-
-/**********************
-  Database Connection information
-  host: This defines the ip address of the server hosting our database.
-		We'll be using `db` as this is the name of the postgres container in our
-		docker-compose.yml file. Docker will translate this into the actual ip of the
-		container for us (i.e. can't be access via the Internet).
-  port: This defines what port we can expect to communicate to our database.  We'll use 5432 to talk with PostgreSQL
-  database: This is the name of our specific database.  From our previous lab,
-		we created the football_db database, which holds our football data tables
-  user: This should be left as postgres, the default user account created when PostgreSQL was installed
-  password: This the password for accessing the database. We set this in the
-		docker-compose.yml for now, usually that'd be in a seperate file so you're not pushing your credentials to GitHub :).
-**********************/
-
 
 
 // set the view engine to ejs
